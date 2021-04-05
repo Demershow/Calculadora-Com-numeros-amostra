2c5867ca58a56f30be563ae291b77d749269ae59
Demershow <demersontorres520@gmail.com>
2021-04-06 05:03:06 +0900

    Programação JS da calculadora

---
+++ www/components/calcul.js
@@ -0,0 +1,59 @@
+// This is a JavaScript file
+var textoDisplay = "";
+
+//Valor 1 das operações
+var valor1 = 0;
+var valor2 = 0;
+var resultado = 0;
+
+//identificar operador do calculo
+var operador = "";
+
+//função para zerar o display
+function zerarDisplay(){
+  textoDisplay = "";
+  $(".display").val("");
+}
+
+//Programação dos numeros
+$(document).on("click",".botao",function(){
+  textoDisplay += $(this).val();
+  $(".display").val(textoDisplay);
+})
+
+//Programação dos operadores
+$(document).on("click", ".OP", function(){
+  operador = $(this).html();
+  valor1 = textoDisplay;
+  zerarDisplay();
+})
+
+//Programação do Botão CE (tentando fazer sozinho)
+$(document).on("click", ".ce", function(){
+  zerarDisplay();
+  $(".display").val("0");
+  valor1 = 0;
+})  //Consegui :))))
+
+$(document).on("click","#igual", function(){
+   valor2 =  textoDisplay;
+   zerarDisplay();
+
+   if(operador == "+"){
+        textoDisplay = parseFloat(valor1) + parseFloat(valor2); 
+        }
+
+   if(operador == "-"){
+        textoDisplay = parseFloat(valor1) - parseFloat(valor2); 
+        }
+
+   if(operador == "/"){
+        textoDisplay = parseFloat(valor1) / parseFloat(valor2); 
+        }
+
+   if(operador == "*"){
+        textoDisplay = parseFloat(valor1) * parseFloat(valor2); 
+        }
+   $(".display").val(textoDisplay);
+})
+
